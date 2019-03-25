613322b7d697fcfc74846d52bee8cf05a2591dbf
Travis Matos <travism7618@hstat.org>
2019-03-25 13:07:51 +0000

    adds comments

--- application_controller.rb
+++ application_controller.rb
@@ -6,15 +6,15 @@ require_relative 'models/model.rb'
 
 class ApplicationController < Sinatra::Base
 
+# this is the starting point, when the site is first accessed 
   get '/' do
     erb :index
   end
   
   post '/result' do
-    @user_date= params[:user_date]
-    @result=Pod.new(@user_date)
-    puts "from controller:"
-    puts @result.title
+    
+  # makes a new instance of the pod class with the date from the user (taken from the html form index.erb) and makes it accessible to result.erb 
+    @result=Pod.new(params[:user_date])
     erb :result
   end
   
--- models/model.rb
+++ models/model.rb
@@ -10,12 +10,15 @@ require 'dotenv/load'
 
 class Pod
     attr_reader  :url,:data_hash,:copyright,:date,:explanation,:media_url,:media_type,:copyright,:title
+    # When a pod instance is intialized it takes in a date 
     def initialize(date)
-        @url="https://api.nasa.gov/planetary/apod?api_key=#{ENV["NASA_API_KEY"]}&date=#{date}"
-        @data_hash= JSON.parse(Net::HTTP.get(URI(@url)))
+        @url="https://api.nasa.gov/planetary/apod?api_key=#{ENV["NASA_API_KEY"]}&date=#{date}" # that date is put into the nasa api url along with the api key from the . env file
+        @data_hash= JSON.parse(Net::HTTP.get(URI(@url))) #the modified url returns a hash, which is taken and saved using this line of code
+        # all items of the hash is saved as an instance variable
         @copyright=data_hash["copyright"]
         @date=data_hash["date"]
         @explanation=data_hash["explanation"] 
+        # some hashes have a hd url and some don't. If there is the option for the hd url then this code saves it as the url for the media, if there is not then this code takes the regular media url 
         if data_hash["hdurl"]==nil
             @media_url=data_hash["url"]
         else
